ddf3ea987088147a1c5785cd838e6f36404b1d3c
Markus Hofbauer <markus.hofbauer@tum.de>
2022-03-05 14:28:46 +0100

    update ci

--- install.sh
+++ install.sh
@@ -9,10 +9,8 @@ echo "Setup View Adaptation"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 WS_SRC="${SCRIPT_DIR}"/..
 
-pushd "$WS_SRC" || exit
-
 # TELECARLA
-git clone https://github.com/hofbi/telecarla.git
+git clone https://github.com/hofbi/telecarla.git "$WS_SRC"/telecarla
 "$WS_SRC"/telecalra/install.sh
 
 echo "Finished setup of View Adpatation"
